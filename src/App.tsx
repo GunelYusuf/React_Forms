ba3b2bad1e49c3e7ed72ff5ef9bb45add5f5fd18
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-01-22 01:18:37 +0400

    adding handleNameChange function

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import {Input} from "./components/Input";
 import './App.css';
 import {Button} from "./components/Button";
 
+
+
+
 function App() {
+    const [name,setName]=React.useState('');
+    const handleNameChange = React.useCallback((event: ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    },[]);
+    console.log('something happened');
   return (
     <div className="App">
       <header className="App-header">
-       <Input/>
+       <Input value={name} onChange={handleNameChange}/>
           <Button>Click me!</Button>
       </header>
     </div>
