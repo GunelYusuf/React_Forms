19993d42d0bcfa892e0a5d0650d3f9ae53937b96
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-01-22 02:29:51 +0400

    feat:create BMI Calculate

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,47 @@ import './App.css';
 import {Button} from "./components/Button";
 
 
+interface IBmiDetails {
+    weight: string;
+    height: string;
+}
 
+enum  EBmiValue {
+    WEIGHT ='weight',
+    HEIGHT='height'
+}
+
+const initialBmiDetails: IBmiDetails = {
+    weight: '',
+    height: ''
+}
 
 function App() {
-    const [name,setName]=React.useState('');
-    const handleNameChange = React.useCallback((event: ChangeEvent<HTMLInputElement>) => {
-        setName(event.target.value);
+    const [bmiDetails,setBmiDetails]=React.useState<IBmiDetails>(initialBmiDetails);
+   
+    const handleBmiDetailsChange = React.useCallback((event: ChangeEvent<HTMLInputElement>,type= EBmiValue.HEIGHT ) => {
+        setBmiDetails(oldValue => ({...oldValue,[type]:event.target.value}));
     },[]);
-    console.log('something happened');
+   const handleCalculateClick = React.useCallback(() =>{
+      const {weight,height} = bmiDetails;
+      const weightAsNumber = Number(weight);
+      const heightAsNumber = Number(height);
+
+      if(isNaN(weightAsNumber) || isNaN(heightAsNumber)){
+          alert('Enter valid values');
+      }else{
+        alert(`Your BMI is: ${(weightAsNumber / Math.pow(heightAsNumber / 100, 2)).toFixed(2)}`);
+          setBmiDetails(initialBmiDetails)
+      }
+   },[bmiDetails]);
+
+
   return (
     <div className="App">
       <header className="App-header">
-       <Input value={name} onChange={handleNameChange}/>
-          <Button>Click me!</Button>
+       <Input value={bmiDetails.weight} placeholder='Weight in kg' onChange={(event) => handleBmiDetailsChange(event, EBmiValue.WEIGHT)}/>
+          <Input value={bmiDetails.height} placeholder = 'Height in cm' onChange={(event) => handleBmiDetailsChange(event)} />
+          <Button onClick={handleCalculateClick}>Calculate</Button>
       </header>
     </div>
   );
